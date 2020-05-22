5e8e9f56a78362c0690085bf5cb6beb3acfe1860
NTP_UDPP <47802531+NTPUDPP@users.noreply.github.com>
2020-05-22 15:02:11 +0700

    Add files via upload

---
+++ xo.py
@@ -0,0 +1,84 @@
+import numpy as np
+from math import inf as infinity
+
+game_state = [['','',''],['','',''],['','','',]]; #2D-Array 
+players = ['X','O']; #Player's types
+
+def move_on(player_ind,state,block_num):
+    if (state[(block_num-1)/3][(block_num-1)%3] is ''):
+        state[(block_num-1)/3][(block_num-1)%3] = player_ind
+    else:
+        block_num = input('Position is not empty!! Please enter another position for placing X or O : ')
+        move_on(player_ind,state,block_num)
+
+def checking_current_state(game_state):
+    #Checking a drawing condition 
+    draw_flag = 0
+    for i in range(3):
+        for j in range(3):
+            if game_state[i][j] == '':
+                draw_flag = 1
+    if draw_flag == 0:
+        return None, "Draw"
+
+    #Checling a winning condition (Vertical)
+    if (game_state[0][0] == game_state[1][0] and game_state[1][0] == game_state[2][0] and game_state[0][0] is not ''): #left-vertical 
+        return game_state[0][0], "Done"
+    if (game_state[0][1] == game_state[1][1] and game_state[1][1] == game_state[2][1] and game_state[0][1] is not ''): #middle-vertical 
+        return game_state[0][1], "Done"
+    if (game_state[0][2] == game_state[1][2] and game_state[1][2] == game_state[2][2] and game_state[0][2] is not ''): #right-vertical 
+        return game_state[0][2], "Done"
+
+    #Checking a winning condition (Horizontal)
+    if (game_state[0][0] == game_state[0][1]and game_state[0][1] == game_state[0][2] and game_state[0][0] is not ''): #top-horizontal
+        return game_state[0][0], "Done"
+    if (game_state[1][0] == game_state[1][1]and game_state[1][1] == game_state[1][2] and game_state[1][0] is not ''): #middle-horizontal
+        return game_state[1][0], "Done"
+    if (game_state[2][0] == game_state[2][1]and game_state[2][1] == game_state[2][2] and game_state[2][0] is not ''): #bottom-horizontal
+        return game_state[2][0], "Done"
+    
+    #Checking a winning condition (Diagonal)
+    if (game_state[0][0] == game_state[1][1]and game_state[1][1] == game_state[2][2] and game_state[0][0] is not ''): #left-horizontal
+        return game_state[0][0], "Done"
+    if (game_state[0][2] == game_state[1][1]and game_state[1][1] == game_state[2][0] and game_state[0][2] is not ''): #right-horizontal
+        return game_state[0][2], "Done"
+
+    return None, "Not yet"
+
+def create_board(game_state):
+    print('____________________'),
+    print('|' + str(game_state[0][0] + '|' + str(game_state[0][1]) + '|' + str(game_state[0][2])), 
+    print('|' + str(game_state[1][0] + '|' + str(game_state[1][1]) + '|' + str(game_state[1][2])),
+    print('|' + str(game_state[2][0] + '|' + str(game_state[2][1]) + '|' + str(game_state[2][2])),
+    print('____________________')
+
+#Playing 
+p_repeat = 'Y'
+while p_repeat == 'Y' or p_repeat == 'y':
+     current_state = "Not yet"
+     game_state = [['','',''],['','',''],['','','',]]; #2D-Array
+     winner = None 
+     player_order = input('Who gonna play it first (X is human or O is bot) : ')
+     if player_order == 'X' or player_order == 'x':
+         player_id = 0 #Human 
+     else:
+         player_id = 1 #Bot
+
+     while current_state == 'Not yet':
+        if player_id == 0: #Human
+            player_pos = input('Enter your position to place X or O (from 1-9) : ')
+            play_move = move_on(players[player_id],game_state,player_pos)
+        else: #Bot
+            player_pos = input('Enter your position to place X or O (from 1-9) : ')
+            play_move = move_on(players[player_id],game_state,player_pos)
+        create_board(game_state)
+
+        winner, current_state = checking_current_state(game_state)
+
+        if (winner is not None): 
+            print(str(winner) + 'is the winner!!')
+        else:
+            player_id = cu
+
+        if (current_state == "Draw"):
+            print('DRAW!!')
